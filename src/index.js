54ba54e17486c936a0284f81790479ca5b82f0d5
flaviogf <flavio.fernandes6@gmail.com>
2020-03-20 09:10:54 -0300

    fix: updating the port that is be listening by the app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => res.status(200).json({ date: new Date() }))
 
-app.listen(3000, () => console.log('It is running!'))
+app.listen(port, () => console.log(`It is running at port: ${port}!`))
